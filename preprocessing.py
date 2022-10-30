5fef9ede939e7d5d534f48c07ce90055d65abba6
capac <capac@users.noreply.github.com>
2022-10-30 11:05:39 +0000

    Created PreProcessor class and NLTKPreProcessor and SpacyPreProcessor subclasses

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,11 @@ import spacy
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import re
+import warnings
+warnings.filterwarnings('ignore')
 
 
-class PreProcessing():
+class PreProcesser():
     def __init__(self, text):
         self.text = text
         # lowercase
@@ -15,18 +17,33 @@ class PreProcessing():
         self.text = re.sub(r'[^\w\s]', '', self.text)
 
 
-class PreProcessWithSpacy(PreProcessing):
+class NLTKPreProcesser(PreProcesser):
     def __init__(self, text):
         super().__init__(text)
         # remove stopwords
-        text = " ".join([w for w in text.split() if w not in
-                        set(stopwords.words("english"))])
+        self.text = " ".join([w for w in self.text.split() if w not in
+                             set(stopwords.words("english"))])
         # get the lemma for each word
-        text = " ".join([WordNetLemmatizer().lemmatize(w) for w in text.split()])
+        self.text = " ".join([WordNetLemmatizer().lemmatize(w) for w in self.text.split()])
 
 
-class PreProcessWithNLTK(PreProcessing):
+class SpacyPreProcesser(PreProcesser):
     def __init__(self, text):
         super().__init__(text)
         nlp = spacy.load("en_core_web_sm", disable=["tok2vec", "textcat", "ner"])
-        return nlp(text)
+        self.text = nlp(self.text)
+        # remove stopwords
+        self.text = [token for token in self.text if not token.is_stop]
+        # get the lemma for each word
+        self.text = [str(token.lemma_) for token in self.text]
+        self.text = " ".join(self.text)
+
+
+if __name__ == "__main__":
+    text = "Hello, how are you today? I hope you don't find this case study too dificult!"
+    preprocess0 = PreProcesser(text)
+    preprocess1 = NLTKPreProcesser(text)
+    preprocess2 = SpacyPreProcesser(text)
+    print(preprocess0.text)
+    print(preprocess1.text)
+    print(preprocess2.text)
