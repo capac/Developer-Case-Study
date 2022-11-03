1feffc7b9bc496de8f75ee1c3d6c0b7be7b67161
capac <capac@users.noreply.github.com>
2022-11-03 14:22:48 +0000

    Removed IntegrityError exception

--- data_app/application.py
+++ data_app/application.py
@@ -1,5 +1,4 @@
 from . import data as d
-from sqlite3 import IntegrityError
 
 
 class Application():
@@ -9,7 +8,7 @@ class Application():
         addition or update and deletion of record'''
 
         # database login
-        self.database_login()
+        self.database_login('reviews.db')
 
         # create database and table if non-existent
         self.data_model.create_db_and_primary_table()
@@ -17,33 +16,30 @@ class Application():
         # import CSV file to database
         self.file_import("Womens Clothing E-Commerce Reviews.csv")
 
-    def database_login(self):
+    def database_login(self, db_name):
         '''Creates database and table for data'''
 
-        self.db_name = 'reviews.db'
+        self.db_name = db_name
         self.data_model = d.SQLModel(self.db_name)
 
     def file_import(self, filename):
         '''Imports CSV file and load records to the database'''
 
-        try:
-            if filename:
-                try:
-                    csv_read = d.CSVModel(filename=filename, filepath='data_app/')
-                except Exception as e:
-                    print('Problem reading file')
-                    raise e
-                else:
-                    try:
-                        records = csv_read.load_records()
-                        for row in records:
-                            self.data_model.add_record(row)
-                    except TypeError:
-                        print('Cannot add data to table')
+        if filename:
+            try:
+                csv_read = d.CSVModel(filename=filename, filepath='data_app/')
+            except Exception as e:
+                print('Problem reading file')
+                raise e
             else:
-                raise Exception("File doesn't exist")
-        except IntegrityError:
-            pass
+                try:
+                    records = csv_read.load_records()
+                    for row in records:
+                        self.data_model.add_record(row)
+                except TypeError:
+                    print('Cannot add data to table')
+        else:
+            raise Exception("File doesn't exist")
 
     def get_record(self, id):
         try:
